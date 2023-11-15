05003ff6afd52058c0168a223659201b675002f1
gabriela-barbosa <gabrielabf@id.uff.br>
2023-11-15 14:59:59 -0300

    chore: update gerenciamento page style

--- src/pages/gerenciamento/index.tsx
+++ src/pages/gerenciamento/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Theme } from '@utils/enums'
 import 'react-toastify/dist/ReactToastify.css'
-import money from '@public/foto1.png'
-import settings from '@public/foto2.jpg'
-import article from '@public/foto3.png'
-import email from '@public/email.png'
-import user from '@public/user.png'
 
 import { useRouter } from 'next/router'
 import MainWrapper from '@components/MainWrapper'
 import { PageWrapper, Title } from '@styles/index.style'
-import { Grid } from '@mui/material'
-import { CardOptions, colors } from '@components'
-
+import { Grid, SvgIconTypeMap } from '@mui/material'
+import { CardOptions } from '@components'
+import SchoolRoundedIcon from '@mui/icons-material/SchoolRounded'
+import BusinessRoundedIcon from '@mui/icons-material/BusinessRounded'
+import HomeWorkRoundedIcon from '@mui/icons-material/HomeWorkRounded'
+import AccountBalanceRoundedIcon from '@mui/icons-material/AccountBalanceRounded'
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded'
+import ManageAccountsRoundedIcon from '@mui/icons-material/ManageAccountsRounded'
+import { OverridableComponent } from '@mui/types'
 enum ManagementLinks {
   PROGRAM = 'programasic',
   INSTITUTION_TYPE = 'tiposdeinstituicao',
@@ -22,6 +23,13 @@ enum ManagementLinks {
   CONFIG_INSTITUTION = 'instituicoes',
 }
 
+interface OptionProps {
+  title: string
+  subtitle: string
+  icon: OverridableComponent<SvgIconTypeMap>
+  onClick: () => void
+}
+
 const Select: React.FC = () => {
   const router = useRouter()
 
@@ -29,53 +37,41 @@ const Select: React.FC = () => {
     router.push(`gerenciamento/${option}`)
   }
 
-  const options = [
+  const options: OptionProps[] = [
     {
       title: 'Configurar Programas',
       subtitle: 'Adicione, exclua ou edite um programa de pós-graduação.',
-      icon: money,
-      altImg: 'money',
-      color: colors.green,
+      icon: AccountBalanceRoundedIcon,
       onClick: () => goToPage(ManagementLinks.PROGRAM),
     },
     {
       title: 'Configurar Instituições',
       subtitle: 'Adicione, exclua ou edite uma instituição.',
-      icon: user,
-      altImg: 'user',
-      color: colors.purple,
+      icon: BusinessRoundedIcon,
       onClick: () => goToPage(ManagementLinks.CONFIG_INSTITUTION),
     },
     {
       title: 'Configurar Tipo de Instituição',
       subtitle: 'Adicione, exclua ou edite um tipo de instituição.',
-      icon: settings,
-      altImg: 'settings',
-      color: colors.red,
+      icon: HomeWorkRoundedIcon,
       onClick: () => goToPage(ManagementLinks.INSTITUTION_TYPE),
     },
     {
       title: 'Configurar Níveis CNPQ',
       subtitle: 'Adicione, exclua ou edite um nível CNPQ.',
-      icon: article,
-      altImg: 'article',
-      color: colors.blue,
+      icon: SchoolRoundedIcon,
       onClick: () => goToPage(ManagementLinks.CNPQ_LEVELS),
     },
     {
       title: 'Configurar Conteúdo do Email',
       subtitle: 'Adicione, exclua ou edite um email.',
-      icon: email,
-      altImg: 'email',
-      color: colors.yellow,
+      icon: EmailRoundedIcon,
       onClick: () => goToPage(ManagementLinks.CONFIG_EMAIL),
     },
     {
       title: 'Configurar Usuários',
       subtitle: 'Adicione, exclua ou edite um usuário.',
-      icon: user,
-      altImg: 'user',
-      color: colors.purple,
+      icon: ManageAccountsRoundedIcon,
       onClick: () => goToPage(ManagementLinks.CONFIG_USER),
     },
   ]
@@ -83,23 +79,24 @@ const Select: React.FC = () => {
   return (
     <>
       <MainWrapper themeName={Theme.white} hasContent={true} hasHeader={true}>
-        <PageWrapper>
-          <Title>Gerenciamento de opções</Title>
-          <br></br>
-          <br></br>
-          <Grid container spacing={8}>
-            {options.map(({ title, subtitle, icon, altImg, color, onClick }, index) => (
-              <Grid item key={index}>
-                <CardOptions
-                  color={color}
-                  onClick={onClick}
-                  altImg={altImg}
-                  icon={icon}
-                  subtitle={subtitle}
-                  title={title}
-                />
-              </Grid>
-            ))}
+        <PageWrapper container rowSpacing={3}>
+          <Grid item>
+            <Title>Gerenciamento de opções</Title>
+          </Grid>
+
+          <Grid item>
+            <Grid container spacing={8}>
+              {options.map(({ title, subtitle, icon, onClick }, index) => (
+                <Grid item key={index}>
+                  <CardOptions
+                    onClick={onClick}
+                    IconComponent={icon}
+                    subtitle={subtitle}
+                    title={title}
+                  />
+                </Grid>
+              ))}
+            </Grid>
           </Grid>
         </PageWrapper>
       </MainWrapper>
